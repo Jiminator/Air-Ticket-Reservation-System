79b167d2038b0ada93030a843753f2c5b2c1d119
Mitchellzhou1 <mitche.zhou1@gmail.com>
2022-11-29 17:22:28 -0500

    Revert "so far"
    This reverts commit 6ce902c8b3700d9bc95d0b64710adaa74cd65eef.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pymysql.cursors
 app = Flask(__name__)
 
 # Configure MySQL
-conn = pymysql.connect(host="localhost",
+conn = pymysql.connect(host="127.0.0.1",
                        port=3306,
                        user='root',
                        password='',
