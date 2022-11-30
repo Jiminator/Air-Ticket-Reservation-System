641aae2c5f72e801548f792f8f9428507486af44
Mitchellzhou1 <mitche.zhou1@gmail.com>
2022-11-30 00:24:02 -0500

    not complete
    SUI

--- customer.py
+++ customer.py
@@ -32,7 +32,7 @@ def logoutCustomer():
     session.pop('email')
     return redirect('customerLogin')
 
-@app.route('/customerPurchase')
+@app.route('/customerPurchase', methods=['GET', 'POST'])
 def customerPurchase():
     cursor = conn.cursor()
     query = """
@@ -46,8 +46,45 @@ def customerPurchase():
     cursor.close()
     return render_template('customerPurchase.html', flights=flightdata)
 
+@app.route('/customerPurchaseUpdate', methods=['GET', 'POST'])
+def customerPurchaseUpdate():
+    email = session['email']
+    flightNo = request.form['ticket_ID']
+    ticket = int(request.form['ticket_ID'])
+    cardType = request.form["card type"]
+    name = request.form['Name of Card']
+    cardNumber = int(request.form['cc-number'])
+    expDate = request.form["expiration start"]
 
-@app.route('/customerviewflights', methods=['GET', 'POST'])
+    cursor = conn.cursor()
+    query = 'SELECT base_price FROM flight NATURAL JOIN ticket WHERE flight_number=%s'
+    cursor.execute(query, (flightNo))
+    cost = cursor.fetchall()
+    print("THISSSSS")
+    print("IT IS A ", cost, type(cost))
+    # NOT COMPLETELY WORKING!!!
+    query = 'INSERT INTO purchase VALUES (1111, %s, 0 ,%s, 3728193847283410, %s, %s, NOW());'
+    cursor.execute(query, (email, cardType, name, expDate))
+    conn.commit()
+    cursor.close()
+
+    return redirect(url_for('customerPurchase'))
+
+    # ensures that the Airline the user entered exists
+    cursor = conn.cursor()
+    query = 'SELECT * FROM airline WHERE airline_name = %s'
+    cursor.execute(query, airline_name)
+    data = cursor.fetchone()
+    ins ='INSERT INTO testing VALUES("SEE THIS WORKEDDDD@gmail.com")'
+    cursor.execute(ins)
+    conn.commit()
+    cursor.close()
+    session['username'] = username
+    # it has to be staff_home
+    return redirect(url_for('staff_home'))
+
+
+@app.route('/customerviewflights')
 def customerviewflights():
     email = session['email']
     cursor = conn.cursor()
@@ -61,6 +98,17 @@ def customerviewflights():
     flightdata = cursor.fetchall()
     cursor.close()
     return render_template('customerviewflights.html', flights=flightdata)
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/customerRateflight', methods=['GET', 'POST'])
