39e3dfa9c5a0d1ef5d065345a794ccfd9b2e51fa
Jiminator <69131491+Jiminator@users.noreply.github.com>
2022-12-08 23:10:47 -0500

    Final changes

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ import pymysql.cursors
 app = Flask(__name__)
 
 # Configure MySQL
-conn = pymysql.connect(host="127.0.0.1",
-                       port=3306,
-                       user='root',
-                       password='',
-                       db='air_ticket_reservation_system',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
-
-# conn = pymysql.connect(host="localhost",
-#                        port=8889,
+# conn = pymysql.connect(host="127.0.0.1",
+#                        port=3306,
 #                        user='root',
-#                        password='root',
+#                        password='',
 #                        db='air_ticket_reservation_system',
 #                        charset='utf8mb4',
 #                        cursorclass=pymysql.cursors.DictCursor)
+
+conn = pymysql.connect(host="localhost",
+                       port=8889,
+                       user='root',
+                       password='root',
+                       db='air_ticket_reservation_system',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 
